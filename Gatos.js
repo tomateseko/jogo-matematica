4c65368fd3372ede944a29cede78e2556c400f1e
tomateseko <vitoria.lustosa@escola.pr.gov.br>
2025-03-12 13:55:59 -0300

    Gatos-011.js

---
+++ Gatos.js
@@ -0,0 +1,7 @@
+// Desenhe quantos gatos quiser!
+var desenharGatos = function (quantasVezes) {
+for (var i = 0; i < quantasVezes; i++) {
+console.log(i + " =^.^=");
+}
+};
+desenharGatos(10); // Você pode trocar o nº de repetições
