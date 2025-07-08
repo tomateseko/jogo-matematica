8dd85c0f8fecc4ffaf8149ecce69af9b4beb4497
tomateseko <vitoria.lustosa@escola.pr.gov.br>
2025-07-08 19:26:40 -0300

    Create resultado-ifelse.js

---
+++ resultado-ifelse.js
@@ -0,0 +1,7 @@
+let avaliacao = 7.50; // Ex. de nota pode ter casa decimal, assim 0.00 - não use vírgula!
+
+if (avaliacao >= 6.00) { // Nota mínima para aprovação
+  console.log("Você está aprovado. Parabéns!");
+} else {
+  console.log("Infelizmente você não foi aprovado.");
+}
